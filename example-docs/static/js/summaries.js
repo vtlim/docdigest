a06d2ea7ec39e654991bc90cc29e142573c437fc
Victoria Lim <lim.t.victoria@gmail.com>
2025-11-07 15:58:36 -0800

    Remove summary for intro

--- example-docs/static/js/summaries.js
+++ example-docs/static/js/summaries.js
@@ -2,11 +2,9 @@
 Summaries for each topic, matched by filename
 */
 
-const intro = "Summary in debug mode. Headers: 5, Word count: 145, Random string: 6WOuo";
 const introduction = "Summary in debug mode. Headers: 5, Word count: 145, Random string: fbPnd";
 
 
 module.exports = {
-  intro,
   introduction
 };
