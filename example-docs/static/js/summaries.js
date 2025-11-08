744ef6ec65a92c9d4f1e7167bfef24965322d56c
Victoria Lim <lim.t.victoria@gmail.com>
2025-11-07 17:18:15 -0800

    Add summary for tutorial_extras_manage_docs_versions

--- example-docs/static/js/summaries.js
+++ example-docs/static/js/summaries.js
@@ -4,9 +4,11 @@ Summaries for each topic, matched by filename
 
 const introduction = "Summary in debug mode. Headers: 5, Word count: 145, Random string: fbPnd";
 const tutorial_extras_translate_your_site = "Summary in debug mode. Headers: 6, Word count: 97, Random string: E5Pry";
+const tutorial_extras_manage_docs_versions = "Summary in debug mode. Headers: 4, Word count: 67, Random string: ec1fK";
 
 
 module.exports = {
   introduction,
-  tutorial_extras_translate_your_site
+  tutorial_extras_translate_your_site,
+  tutorial_extras_manage_docs_versions
 };
