d0d5953791b6446d47f8039d9ae6e17249df96f2
Victoria Lim <lim.t.victoria@gmail.com>
2025-11-07 17:18:15 -0800

    Add summary for tutorial_extras_translate_your_site

--- example-docs/static/js/summaries.js
+++ example-docs/static/js/summaries.js
@@ -3,8 +3,10 @@ Summaries for each topic, matched by filename
 */
 
 const introduction = "Summary in debug mode. Headers: 5, Word count: 145, Random string: fbPnd";
+const tutorial_extras_translate_your_site = "Summary in debug mode. Headers: 6, Word count: 97, Random string: E5Pry";
 
 
 module.exports = {
-  introduction
+  introduction,
+  tutorial_extras_translate_your_site
 };
