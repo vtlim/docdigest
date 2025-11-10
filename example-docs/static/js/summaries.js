d664d49afab6a768dba092c2766849645b0c012d
Victoria Lim <lim.t.victoria@gmail.com>
2025-11-10 13:54:55 -0800

    Add summary for tutorial_basics_congratulations

--- example-docs/static/js/summaries.js
+++ example-docs/static/js/summaries.js
@@ -6,11 +6,13 @@ const introduction = "Summary in debug mode. Headers: 5, Word count: 145, Random
 const tutorial_extras_translate_your_site = "Summary in debug mode. Headers: 6, Word count: 97, Random string: E5Pry";
 const tutorial_extras_manage_docs_versions = "Summary in debug mode. Headers: 4, Word count: 67, Random string: ec1fK";
 const tutorial_basics_create_a_page = "Summary in debug mode. Headers: 3, Word count: 38, Random string: csDsW";
+const tutorial_basics_congratulations = "Summary in debug mode. Headers: 2, Word count: 43, Random string: Hk29E";
 
 
 module.exports = {
   introduction,
   tutorial_extras_translate_your_site,
   tutorial_extras_manage_docs_versions,
-  tutorial_basics_create_a_page
+  tutorial_basics_create_a_page,
+  tutorial_basics_congratulations
 };
