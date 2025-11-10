b2ba719b90ac9d7b7102a05183588c549be31686
Victoria Lim <lim.t.victoria@gmail.com>
2025-11-10 13:54:55 -0800

    Add summary for tutorial_basics_create_a_page

--- example-docs/static/js/summaries.js
+++ example-docs/static/js/summaries.js
@@ -5,10 +5,12 @@ Summaries for each topic, matched by filename
 const introduction = "Summary in debug mode. Headers: 5, Word count: 145, Random string: fbPnd";
 const tutorial_extras_translate_your_site = "Summary in debug mode. Headers: 6, Word count: 97, Random string: E5Pry";
 const tutorial_extras_manage_docs_versions = "Summary in debug mode. Headers: 4, Word count: 67, Random string: ec1fK";
+const tutorial_basics_create_a_page = "Summary in debug mode. Headers: 3, Word count: 38, Random string: csDsW";
 
 
 module.exports = {
   introduction,
   tutorial_extras_translate_your_site,
-  tutorial_extras_manage_docs_versions
+  tutorial_extras_manage_docs_versions,
+  tutorial_basics_create_a_page
 };
