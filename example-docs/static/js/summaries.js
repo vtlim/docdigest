33a9f68f8c45952b7604960586ab9d8a0de092b1
Victoria Lim <vtlim@users.noreply.github.com>
2025-11-10 14:36:29 -0800

    Docs output for include a dir, exclude a dir (#3)

--- example-docs/static/js/summaries.js
+++ example-docs/static/js/summaries.js
@@ -2,13 +2,19 @@
 Summaries for each topic, matched by filename
 */
 
-const introduction = "Summary in debug mode. Headers: 5, Word count: 145, Random string: fbPnd";
-const tutorial_extras_translate_your_site = "Summary in debug mode. Headers: 6, Word count: 97, Random string: E5Pry";
-const tutorial_extras_manage_docs_versions = "Summary in debug mode. Headers: 4, Word count: 67, Random string: ec1fK";
+const tutorial_basics_create_a_page = "Summary in debug mode. Headers: 3, Word count: 38, Random string: csDsW";
+const tutorial_basics_congratulations = "Summary in debug mode. Headers: 2, Word count: 43, Random string: Hk29E";
+const tutorial_basics_create_a_blog_post = "Summary in debug mode. Headers: 2, Word count: 34, Random string: 1GRuu";
+const tutorial_basics_create_a_document = "Summary in debug mode. Headers: 5, Word count: 49, Random string: sq9E7";
+const tutorial_basics_deploy_your_site = "Summary in debug mode. Headers: 3, Word count: 66, Random string: iGZPT";
+const tutorial_basics_test = "Summary in debug mode. Headers: 0, Word count: 0, Random string: 8aUgo";
 
 
 module.exports = {
-  introduction,
-  tutorial_extras_translate_your_site,
-  tutorial_extras_manage_docs_versions
+  tutorial_basics_create_a_page,
+  tutorial_basics_congratulations,
+  tutorial_basics_create_a_blog_post,
+  tutorial_basics_create_a_document,
+  tutorial_basics_deploy_your_site,
+  tutorial_basics_test
 };
